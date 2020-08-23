9c1bbe8bb548030d8b402cd80dc0bec25f9e1e2d
Vladoxx <wladox@outlook.com>
2020-08-23 20:40:23 -0300

    Inicial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { UsuarioService } from './usuario.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formulario';
+
+  usuario: any = {};
+
+  constructor(private usuService: UsuarioService) {
+    // usuService.getUsuario().subscribe(usuario => this.usuario = usuario);
+  }
+
+  onSubmit(formulario) {
+    if(formulario.form.status == 'INVALID')
+      alert('formulário inválido! Os dados estão incorretos!');
+    
+
+    this.usuService.postUsuario(formulario.form.value)
+    .subscribe(resposta => {
+      console.log(resposta);
+      alert('Dados enviados com sucesso!')
+    })
+    
+      
+   // console.log(formulario.form.value);
+  }
 }
